4a23b5abce700ac5ef4e3a5e824548d17273a2a6
jjdone <ehdlfaos@gmail.com>
2023-02-07 13:52:10 +0900

    init commit

---
+++ src/main/java/simple/lck/LckApplication.java
@@ -0,0 +1,13 @@
+package simple.lck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LckApplication.class, args);
+	}
+
+}
