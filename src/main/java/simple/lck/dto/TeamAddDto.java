d680a265c6555f5241f73894808ad19eae22a11d
jjdone <ehdlfaos@gmail.com>
2023-02-09 17:30:39 +0900

    Team add DTO 구현
    팀을 추가하기 위해 Team 엔티티와 어시 코치 이름들이 포함됨

--- src/main/java/simple/lck/dto/TeamAddDto.java
+++ src/main/java/simple/lck/dto/TeamAddDto.java
@@ -1,2 +1,18 @@
-package simple.lck.dto;public class TeamAddDto {
+package simple.lck.dto;
+
+import lombok.Data;
+
+@Data
+public class TeamAddDto {
+
+    private Long teamId;
+    private String name;
+    private String headCoach;
+    private int won;
+    private int lost;
+    private int score;
+    private String detailUrl;
+    private String assistantCoach1;
+    private String assistantCoach2;
+    private String assistantCoach3;
 }
