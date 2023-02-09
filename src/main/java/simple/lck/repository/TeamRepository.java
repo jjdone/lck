38f29e22e36504793769acf6fe4e04739e0a3090
jjdone <ehdlfaos@gmail.com>
2023-02-09 17:37:14 +0900

    fix: 리스트로 바꿈

--- src/main/java/simple/lck/repository/TeamRepository.java
+++ src/main/java/simple/lck/repository/TeamRepository.java
@@ -3,5 +3,8 @@ package simple.lck.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import simple.lck.domain.Team;
 
+import java.util.List;
+
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    List<Team> findAllByName(String teamName);
 }
