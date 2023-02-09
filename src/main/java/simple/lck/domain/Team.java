ff5f3279e462f9c0eace9363a851c08954b2a6d0
jjdone <ehdlfaos@gmail.com>
2023-02-09 17:36:06 +0900

    비즈니스 로직 및 필요 메서드 구현
    어시 코치 연관관계 메서드 구현, 점수 업데이트 메서드 구현

--- src/main/java/simple/lck/domain/Team.java
+++ src/main/java/simple/lck/domain/Team.java
@@ -1,14 +1,19 @@
 package simple.lck.domain;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import simple.lck.dto.TeamUpdateScoreDto;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
+@Setter
+@NoArgsConstructor
 public class Team {
 
     @Id @GeneratedValue
@@ -22,4 +27,32 @@ public class Team {
     private int lost;
     private int score;
     private String detailUrl;
+
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL)
+    private List<AssistantCoach> assistantCoaches = new ArrayList<>();
+
+    @Builder
+    public Team(String name, String headCoach, int won, int lost, int score, String detailUrl) {
+        this.name = name;
+        this.headCoach = headCoach;
+        this.won = won;
+        this.lost = lost;
+        this.score = score;
+        this.detailUrl = detailUrl;
+    }
+
+    // 연관관계 메서드
+    public void addAssistantCoach(List<AssistantCoach> assistantCoaches) {
+        for (AssistantCoach assistantCoach : assistantCoaches) {
+            this.assistantCoaches.add(assistantCoach);
+            assistantCoach.setTeam(this);
+        }
+    }
+
+    // 승/페 점수 업데이트
+    public void updateScore(TeamUpdateScoreDto teamUpdateScoreDto) {
+        this.won = teamUpdateScoreDto.getWon();
+        this.lost = teamUpdateScoreDto.getLost();
+        this.score = teamUpdateScoreDto.getScore();
+    }
 }
