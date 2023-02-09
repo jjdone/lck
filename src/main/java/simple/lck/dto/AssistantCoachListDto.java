81199d593e50bb9ef760c80e317a720cb8188431
jjdone <ehdlfaos@gmail.com>
2023-02-09 17:29:27 +0900

    Assistant coach name list DTO 구현
    이름들을 받아와서 AssistantCoach 테이블에 저장하기 위함

--- src/main/java/simple/lck/dto/AssistantCoachListDto.java
+++ src/main/java/simple/lck/dto/AssistantCoachListDto.java
@@ -1,2 +1,11 @@
-package simple.lck.dto;public class AssistantCoachListDto {
+package simple.lck.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class AssistantCoachListDto {
+
+    private List<String> nameList;
 }
