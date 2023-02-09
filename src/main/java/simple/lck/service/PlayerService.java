b978263e6ec3880aee32d786c1ecd18d53f8588f
jjdone <ehdlfaos@gmail.com>
2023-02-09 11:23:32 +0900

    Player 등록 및 삭제 기능 구현

--- src/main/java/simple/lck/service/PlayerService.java
+++ src/main/java/simple/lck/service/PlayerService.java
@@ -1,2 +1,51 @@
-package simple.lck.service;public class PlayerService {
+package simple.lck.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import simple.lck.domain.Player;
+import simple.lck.repository.PlayerRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PlayerService {
+
+    private final PlayerRepository playerRepository;
+
+    @Transactional
+    public Long registration(Player player) {
+        validateDuplicatePlayer(player);
+        playerRepository.save(player);
+        return player.getId();
+    }
+
+    private void validateDuplicatePlayer(Player player) {
+        List<Player> findPlayers = playerRepository.findAllByNickname(player.getNickname());
+        if (!findPlayers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    @Transactional
+    public void delete(Player player) {
+        validateNonexistentPlayer(player);
+        playerRepository.delete(player);
+    }
+
+    private void validateNonexistentPlayer(Player player) {
+        List<Player> findPlayers = playerRepository.findAllByNickname(player.getNickname());
+        if (findPlayers.isEmpty()) {
+            throw new IllegalStateException("존재하지 않는 회원입니다.");
+        }
+    }
+
+    public List<Player> findPlayers() {
+        return playerRepository.findAll();
+    }
+
+    public Player findOne(Long playerId) {
+        return playerRepository.findById(playerId).get();
+    }
 }
