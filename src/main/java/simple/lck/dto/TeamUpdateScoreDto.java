92e348b209556e964026bc6576d6ece03f2f4418
jjdone <ehdlfaos@gmail.com>
2023-02-09 17:31:42 +0900

    TeamUpdateScore DTO 구현
    승/패, 득점 갱신을 위한 DTO

--- src/main/java/simple/lck/dto/TeamUpdateScoreDto.java
+++ src/main/java/simple/lck/dto/TeamUpdateScoreDto.java
@@ -1,2 +1,11 @@
-package simple.lck.dto;public class TeamUpdateScoreDto {
+package simple.lck.dto;
+
+import lombok.Data;
+
+@Data
+public class TeamUpdateScoreDto {
+
+    private int won;
+    private int lost;
+    private int score;
 }
