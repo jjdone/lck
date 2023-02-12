8a99faa28a50ab35391e93ab9a7c0291a574dc43
jjdone <ehdlfaos@gmail.com>
2023-02-12 15:56:37 +0900

    순위(rank) controller와 간단한 html 구현

--- src/main/java/simple/lck/controller/RankController.java
+++ src/main/java/simple/lck/controller/RankController.java
@@ -1,2 +1,24 @@
-package simple.lck.controller;public class RankController {
+package simple.lck.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import simple.lck.domain.Team;
+import simple.lck.service.TeamService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class RankController {
+
+    private final TeamService teamService;
+
+    @GetMapping("/rank")
+    public String rankList(Model model) {
+        List<Team> rank = teamService.findTeamRank();
+        model.addAttribute("rank", rank);
+        return "rank/rankList";
+    }
 }
