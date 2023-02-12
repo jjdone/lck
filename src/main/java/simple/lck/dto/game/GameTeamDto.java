0febf8e19e1ff69bba83cb3f1e58d80af2a1c7a4
jjdone <ehdlfaos@gmail.com>
2023-02-12 13:44:34 +0900

    경기 업데이트를 위한 DTO 추가

--- src/main/java/simple/lck/dto/game/GameTeamDto.java
+++ src/main/java/simple/lck/dto/game/GameTeamDto.java
@@ -1,2 +1,17 @@
-package simple.lck.dto.game;public class GameTeamDto {
+package simple.lck.dto.game;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import simple.lck.domain.Team;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class GameTeamDto {
+
+    private Team team;
+    private int point;
 }
--- src/main/java/simple/lck/dto/game/GameTeamUpdateDto.java
+++ src/main/java/simple/lck/dto/game/GameTeamUpdateDto.java
@@ -1,14 +1,19 @@
 package simple.lck.dto.game;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import simple.lck.configuration.GameState;
 import simple.lck.domain.Team;
-
 
 @Data
 @Builder
-public class GameTeamDto {
+@AllArgsConstructor
+public class GameTeamUpdateDto {
 
-    private Team team;
-    private int point;
+    private Team team1;
+    private int point1;
+    private Team team2;
+    private int point2;
+    private GameState gameState;
 }
