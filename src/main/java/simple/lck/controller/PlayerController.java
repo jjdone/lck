d1e7672cf89f029847be10b00f8c603dbd52a6dd
jjdone <ehdlfaos@gmail.com>
2023-02-13 15:41:14 +0900

    선수 등록, 선수 리스트 조회 구현

--- src/main/java/simple/lck/controller/PlayerController.java
+++ src/main/java/simple/lck/controller/PlayerController.java
@@ -1,2 +1,61 @@
-package simple.lck.controller;public class PlayerController {
+package simple.lck.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import simple.lck.configuration.Position;
+import simple.lck.domain.Player;
+import simple.lck.domain.Team;
+import simple.lck.dto.PlayerAddDto;
+import simple.lck.service.PlayerService;
+import simple.lck.service.TeamService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin/players")
+public class PlayerController {
+
+    private final PlayerService playerService;
+    private final TeamService teamService;
+
+    @GetMapping("/addPlayerForm")
+    public String addPlayerForm(Model model) {
+        List<Team> teams = teamService.findTeams();
+
+        model.addAttribute("teams", teams);
+        model.addAttribute("form", new Player());
+        return "admin/players/addPlayerForm";
+    }
+
+    @ModelAttribute("position")
+    public Position[] position() {
+        return Position.values();
+    }
+
+    @PostMapping("/addPlayerForm")
+    public String addPlayer(@ModelAttribute PlayerAddDto playerAddDto) {
+        Team team = teamService.findOne(playerAddDto.getTeamId());
+        Player player = Player.builder()
+                .name(playerAddDto.getName())
+                .nickname(playerAddDto.getNickname())
+                .position(playerAddDto.getPosition())
+                .team(team)
+                .detailUrl(playerAddDto.getDetailUrl())
+                .build();
+        playerService.addPlayer(player);
+        return "redirect:/admin/players";
+    }
+
+    @GetMapping
+    public String playerList(Model model) {
+        List<Player> players = playerService.findPlayers();
+        model.addAttribute("players", players);
+        return "admin/players/playerList";
+    }
 }
