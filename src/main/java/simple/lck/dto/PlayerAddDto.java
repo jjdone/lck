5e45f4dd0bd82af2da06f5580a4c489f5ec24de2
jjdone <ehdlfaos@gmail.com>
2023-02-13 15:40:29 +0900

    선수 등록하기 위한 DTO 생성

--- src/main/java/simple/lck/dto/PlayerAddDto.java
+++ src/main/java/simple/lck/dto/PlayerAddDto.java
@@ -1,2 +1,15 @@
-package simple.lck.dto;public class PlayerAddDto {
+package simple.lck.dto;
+
+import lombok.Data;
+import simple.lck.configuration.Position;
+import simple.lck.domain.Team;
+
+@Data
+public class PlayerAddDto {
+
+    private String name;
+    private String nickname;
+    private Position position;
+    private Long teamId;
+    private String detailUrl;
 }
