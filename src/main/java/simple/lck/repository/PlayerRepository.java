0a19ab5a4ea4afd2422ea8bd9bfcbaa96b0317a0
jjdone <ehdlfaos@gmail.com>
2023-02-19 16:48:42 +0900

    업데이트 쿼리 추가

--- src/main/java/simple/lck/repository/PlayerRepository.java
+++ src/main/java/simple/lck/repository/PlayerRepository.java
@@ -1,10 +1,18 @@
 package simple.lck.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import simple.lck.domain.Player;
+import simple.lck.domain.Team;
 
 import java.util.List;
 
 public interface PlayerRepository extends JpaRepository<Player, Long> {
     List<Player> findAllByNickname(String nickname);
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = "update Player p set p.team = :team, p.pogPoint = :pogPoint where p.id = :playerId")
+    int updatePlayer(@Param("team") Team team, @Param("pogPoint") int pogPoint, @Param("playerId") Long playerId);
 }
