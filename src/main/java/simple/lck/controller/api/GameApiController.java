b61d584def5123da72418d8c7e8bad4554f95ec4
jjdone <ehdlfaos@gmail.com>
2023-02-21 16:59:13 +0900

    Game List API 넘겨주기

--- src/main/java/simple/lck/controller/api/GameApiController.java
+++ src/main/java/simple/lck/controller/api/GameApiController.java
@@ -1,4 +1,26 @@
 package simple.lck.controller.api;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import simple.lck.dto.game.GameDto;
+import simple.lck.dto.game.GameScheduleDto;
+import simple.lck.service.GameService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/games")
 public class GameApiController {
+
+    private final GameService gameService;
+
+    @GetMapping
+    public ResponseEntity<?> gameList() {
+        List<GameDto> games = gameService.findGameDtoList();
+        return ResponseEntity.ok(games);
+    }
 }
