1325777646dbfa424cf58537e7a122a663be3179
jjdone <ehdlfaos@gmail.com>
2023-02-21 15:51:36 +0900

    Player List API 넘겨주기

--- src/main/java/simple/lck/controller/api/PlayerApiController.java
+++ src/main/java/simple/lck/controller/api/PlayerApiController.java
@@ -1,4 +1,26 @@
 package simple.lck.controller.api;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import simple.lck.dto.player.PlayerDto;
+import simple.lck.service.PlayerService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/players")
 public class PlayerApiController {
+
+    private final PlayerService playerService;
+
+    @GetMapping
+    public ResponseEntity<?> playerList() {
+        List<PlayerDto> playerDtoList = playerService.findPlayerDtoList();
+        return ResponseEntity.ok(playerDtoList);
+    }
 }
