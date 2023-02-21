949707549f9a1cfbdd984f4df864019d77368adc
jjdone <ehdlfaos@gmail.com>
2023-02-21 15:30:08 +0900

    Main API 넘겨주기

--- src/main/java/simple/lck/controller/api/MainApiController.java
+++ src/main/java/simple/lck/controller/api/MainApiController.java
@@ -1,4 +1,23 @@
 package simple.lck.controller.api;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import simple.lck.dto.game.GameBeforeDto;
+import simple.lck.service.GameService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class MainApiController {
+
+    private final GameService gameService;
+
+    @GetMapping
+    public ResponseEntity<?> main() {
+        GameBeforeDto main = gameService.findMain();
+        return ResponseEntity.ok(main);
+    }
 }
